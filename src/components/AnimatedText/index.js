4e0a13de577c9f76c997618cd027240319202760
pikokr <parangee9706@gmail.com>
2020-11-18 22:39:46 +0900

    backup

--- src/components/AnimatedText/index.js
+++ src/components/AnimatedText/index.js
@@ -3,12 +3,13 @@ import styles from './styles.module.css'
 
 /**
  * @param text {string}
+ * @param delay {number?}
  */
-const AnimatedText = ({text}) => {
+const AnimatedText = ({text, delay=0}) => {
     return (
         text.split('').map((c,i)=>(
             <span aria-hidden="true" key={i} style={{
-                'animation-delay': (.1 + i / 10) + 's'
+                'animation-delay': (.1 + i / 10 + delay) + 's'
             }} className={styles.letter}>{c}</span>
         ))
     );
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import classes from './styles.module.scss'
+import AnimatedText from "../../components/AnimatedText";
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <section className={classes.hero}>
-                    홈
+                    <h1 style={{fontSize: '3rem'}}>
+                        <AnimatedText text="Team Peanut"/>
+                    </h1>
+                    <h1 style={{fontSize: '3rem'}}>
+                        <AnimatedText text="뭔가 하는 팀" delay={1}/>
+                    </h1>
                 </section>
             </div>
         );
