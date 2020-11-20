25960ee8686c6d4f276b77abb3c482a10f1fbe77
pikokr <parangee9706@gmail.com>
2020-11-20 21:54:37 +0900

    eval route for bot

--- bot/src/index.js
+++ bot/src/index.js
@@ -6,4 +6,10 @@ const c = require('../../config.json')
 
 const app = express()
 
-app.listen(c.internal.bot_port)
+const client = new d.Client()
+
+app.post('/eval',async (req, res) => {
+    res.json({result: await eval(req.body)})
+})
+
+app.listen(c.internal.bot_port, '127.0.0.1', () => client.login(c.internal.bot_token))
